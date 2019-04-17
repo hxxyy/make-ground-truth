0adedf079a65d22c696ecb01133833c95628584b
hxxyy <1483551722@qq.com>
2019-04-17 14:25:02 +0800

    try to smooth result

--- gt.py
+++ gt.py
@@ -1,6 +1,7 @@
 import cv2
 import numpy as np
 
+
 def darker(v):
         if v < 127:
                 return 0.01
@@ -27,10 +28,26 @@ img_hsv1 = cv2.cvtColor(img1,cv2.COLOR_BGR2HSV)
 img_hsv2 = cv2.cvtColor(img2,cv2.COLOR_BGR2HSV)
  
 res = img_hsv1.copy()
- 
+# t1 = darker(img_hsv1[:, :, 2][0][0]) 
+# t2 = lighter(img_hsv2[:, :, 2][0][0])
+# t3 = middle(img_hsv1[:, :, 2][0][0])
 for i in range(res[:, :, 2].shape[0]):
     for j in range(res[:, :, 2].shape[1]):
-        res[i][j] = (img_hsv1[i][j] * darker(img_hsv2[:, :, 2][i][j])  + img_hsv2[i][j]  * lighter(img_hsv2[:, :, 2][i][j]) + img_hsv2[i][j] * middle(img_hsv2[:, :, 2][i][j])) /( darker(img_hsv2[:, :, 2][i][j]) + lighter(img_hsv2[:, :, 2][i][j]) + middle(img_hsv2[:, :, 2][i][j]))
+
+        p1 = darker(img_hsv1[:, :, 2][i][j]) 
+        p2 = lighter(img_hsv2[:, :, 2][i][j])
+        p3 = middle(img_hsv1[:, :, 2][i][j])
+        # if abs(p1-t1)>0.5 or abs(p2-t2)>0.5 or abs(p3-t3)>0.5:
+        #         p1 = (p1 + t1) / 2
+        #         p2 = (p2 + t2) / 2
+        #         p3 = (p3 + t3) / 2
+
+        res[i][j] = (img_hsv1[i][j] * p1 + img_hsv2[i][j]  * p2 + img_hsv1[i][j] * p3 ) /( p1 + p2 + p3)
+
+        # t1 = p1
+        # t2 = p2
+        # t3 = p3
+
 
 res = cv2.cvtColor(res, cv2.COLOR_HSV2BGR)
 cv2.imwrite('gt.jpg', res)
